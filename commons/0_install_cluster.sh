9b589276b21c755c1340dc3133d148c08e216d9b
Serge Bishyr <seriybg@gmail.com>
2020-02-09 13:42:21 +0200

    fix timeouts

--- commons/0_install_cluster.sh
+++ commons/0_install_cluster.sh
@@ -22,3 +22,18 @@ kubectl label namespace default istio-injection=enabled;
 
 sleep 120;
 
+CURRENT_LOCATION="$(dirname $0)/";
+export UI_SERVICE="${CURRENT_LOCATION}../explorer-ui/"
+export EVENT_SERVICE="${CURRENT_LOCATION}../event-service/"
+export GROUND_EVENT_SERVICE="${CURRENT_LOCATION}../ground-event-service/"
+export PLAYER_SERVICE="${CURRENT_LOCATION}../player-service/"
+
+kubectl apply -f ${CURRENT_LOCATION}/redis.yaml
+kubectl apply -f ${UI_SERVICE}/explorer-ui.yaml
+kubectl apply -f ${EVENT_SERVICE}/event-service.yaml
+kubectl apply -f ${GROUND_EVENT_SERVICE}/ground-event-service.yaml
+kubectl apply -f ${PLAYER_SERVICE}/player-service.yaml
+kubectl apply -f ${CURRENT_LOCATION}/gateway.yaml
+
+kubectl -n istio-system get svc istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}'
+
---
+++ commons/aliases.sh
@@ -0,0 +1,5 @@
+export CURRENT_LOCATION="$(dirname $0)/";
+export UI_SERVICE="${CURRENT_LOCATION}../explorer-ui/"
+export EVENT_SERVICE="${CURRENT_LOCATION}../event-service/"
+export GROUND_EVENT_SERVICE="${CURRENT_LOCATION}../ground-event-service/"
+export PLAYER_SERVICE="${CURRENT_LOCATION}../player-service/"
