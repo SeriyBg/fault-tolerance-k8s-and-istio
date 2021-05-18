9fd1fa2afdce1d5f64be2f365c17f4ba7ea829b0
Serge Bishyr <seriybg@gmail.com>
2021-05-18 13:53:21 +0300

    update version

--- commons/0_install_cluster.sh
+++ commons/0_install_cluster.sh
@@ -1,13 +1,8 @@
 #!/bin/bash -v
 
-PROJECT_ID=${GKE_PORJECT_ID};
-
 # creating k8s cluster on GKE
-gcloud container clusters create istio-demo --project=${PROJECT_ID} \
-    --machine-type=n1-standard-2 \
-    --num-nodes=3 \
-    --zone europe-west1-b \
-    --no-enable-legacy-authorization;
+gcloud container clusters create istio-demo --num-nodes=2;
+gcloud container clusters get-credentials istio-demo;
 
 # add permissions to the GKE user
 kubectl create clusterrolebinding cluster-admin-binding \
@@ -15,7 +10,7 @@ kubectl create clusterrolebinding cluster-admin-binding \
   --user="$(gcloud config get-value core/account)";
 
 # istall istio
-istioctl install --set profile=demo;
+istioctl install --set profile=demo -y;
 istioctl verify-install;
 kubectl label namespace default istio-injection=enabled --overwrite=true;
 
@@ -27,15 +22,19 @@ export EVENT_SERVICE="${CURRENT_LOCATION}../event-service/"
 export GROUND_EVENT_SERVICE="${CURRENT_LOCATION}../ground-event-service/"
 export PLAYER_SERVICE="${CURRENT_LOCATION}../player-service/"
 
-kubectl apply -f ${CURRENT_LOCATION}/redis.yaml
+# install hazelcast
+helm repo add hazelcast https://hazelcast-charts.s3.amazonaws.com/
+helm repo update
+helm install hz-hazelcast hazelcast/hazelcast
+
 kubectl apply -f ${UI_SERVICE}/explorer-ui.yaml
 kubectl apply -f ${EVENT_SERVICE}/event-service.yaml
 kubectl apply -f ${GROUND_EVENT_SERVICE}/ground-event-service.yaml
 kubectl apply -f ${PLAYER_SERVICE}/player-service.yaml
 kubectl apply -f ${CURRENT_LOCATION}/gateway.yaml
 
-kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.7/samples/addons/grafana.yaml
-kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.7/samples/addons/jaeger.yaml
-kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.7/samples/addons/prometheus.yaml
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.9/samples/addons/grafana.yaml
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.9/samples/addons/jaeger.yaml
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.9/samples/addons/prometheus.yaml
 
 kubectl -n istio-system get svc istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}'
